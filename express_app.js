8146b8121e83356fec6bde1f9d0a512fcc80ac95
ninouz ninou <fedoulmednadjib@gmail.com>
2014-11-13 11:29:33 +0100

    first commit

---
+++ express_app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));//flower.jpg /direname le dossier courant
+
+app.set('views', __dirname +'/views');
+
+exports.app=app;
